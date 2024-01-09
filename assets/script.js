e2825afe3b19d979893df65f581c1b4f353f9b1f
Shawki45 <shawkihaifa@yahoo.com>
2024-01-09 13:49:39 -0500

    added some javascript

---
+++ assets/script.js
@@ -0,0 +1,24 @@
+const url =
+  "https://floating-headland-95050.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972";
+
+// Your API token or credentials here
+const apiToken = "ToyYqIZSbvEarVjWfZaLj1jawbX6kwF6kFKciXRV5WPK5zC9EzSvkqMQ7i36QZ9feLDODdIzqqSTqHHiGg8ZMITPktE1BGeHbVHL26ekB_CpMBTYLfGv0EJGJImdZXYx";
+
+// Making a fetch call with an Authorization header
+fetch(url, {
+  method: "GET", // or 'POST' if required
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: "Bearer " + apiToken,
+  },
+})
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.json(); // we only get here if there is no error
+  })
+  .then((data) => console.log(data))
+  .catch((error) =>
+    console.error("There has been a problem with your fetch operation:", error)
+  );
