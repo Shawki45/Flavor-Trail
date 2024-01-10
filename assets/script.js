7b5dc41f5f548b68d22c54103c3e3c2018bdcfb3
KvnHunt <huntk.twitch@gmail.com>
2024-01-10 12:45:23 -0500

    city List Text

--- assets/script.js
+++ assets/script.js
@@ -2,8 +2,9 @@ const url =
   "https://floating-headland-95050.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=delis&latitude=37.786882&longitude=-122.399972";
 
 // Your API token or credentials here
-const apiToken = "ToyYqIZSbvEarVjWfZaLj1jawbX6kwF6kFKciXRV5WPK5zC9EzSvkqMQ7i36QZ9feLDODdIzqqSTqHHiGg8ZMITPktE1BGeHbVHL26ekB_CpMBTYLfGv0EJGJImdZXYx";
 const apiPlaces = "AIzaSyCL7avHl4kUCwKaLbRkxAkClyjYjbEzq-U"
+const apiToken = "ToyYqIZSbvEarVjWfZaLj1jawbX6kwF6kFKciXRV5WPK5zC9EzSvkqMQ7i36QZ9feLDODdIzqqSTqHHiGg8ZMITPktE1BGeHbVHL26ekB_CpMBTYLfGv0EJGJImdZXYx";
+
 // Making a fetch call with an Authorization header
 fetch(url, {
   method: "GET", // or 'POST' if required
@@ -22,3 +23,12 @@ fetch(url, {
   .catch((error) =>
     console.error("There has been a problem with your fetch operation:", error)
   );
+
+  $(".search_btn").on("click",()=>{ 
+    
+    console.log($(".city").val());
+  });
+
+  console.log(".city")
+  
+  
